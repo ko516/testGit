fed4d710bf1956d39b6989562ff5227ab4f3fcbf
shiraishi_note <shiraishi_note@18.189.52.219>
2017-04-26 16:25:27 -0400

    test2

--- src/ttttt/Sample.java
+++ src/ttttt/Sample.java
@@ -5,6 +5,11 @@ public class Sample {
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
 		System.out.println("test");
+		System.out.println("test");
+		System.out.println("test");
+		System.out.println("test");
+		System.out.println("test");
+
 	}
 
 }
